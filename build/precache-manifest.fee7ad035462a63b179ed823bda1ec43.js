4feb405fb7ccb527749311ccaabc5eaf2171363a
ezio17 <xd_ezio@ukr.net>
2019-08-09 20:53:11 +0300

    build add

---
+++ build/precache-manifest.fee7ad035462a63b179ed823bda1ec43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a74347b6c7027534b9d1dd36fc837fbc",
+    "url": "/posts-react/build/index.html"
+  },
+  {
+    "revision": "ce012eb504e1e784222a",
+    "url": "/posts-react/build/static/css/main.d62a81d8.chunk.css"
+  },
+  {
+    "revision": "5cd6d4dd3edf355e9fc2",
+    "url": "/posts-react/build/static/js/2.396b059e.chunk.js"
+  },
+  {
+    "revision": "ce012eb504e1e784222a",
+    "url": "/posts-react/build/static/js/main.c5e58d6c.chunk.js"
+  },
+  {
+    "revision": "d321997b32a94fcf8605",
+    "url": "/posts-react/build/static/js/runtime~main.67564db4.js"
+  }
+]);
